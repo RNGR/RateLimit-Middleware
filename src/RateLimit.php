de1a82454e80920391d2d1672e82cfaa456e1716
Welling Guzman <hola@wellingguzman.com>
2016-03-23 18:52:23 -0400

    update rate limit

--- src/RateLimit.php
+++ src/RateLimit.php
@@ -13,96 +13,146 @@ class RateLimit extends Middleware
     protected $root = '';
     protected $limit = 100;
     protected $period = 3600;
+    protected $successCallback;
+    protected $failCallback;
+    protected $remaining;
+    protected $reset;
 
-    public function __construct(StorageInterface $storage, $root = '', $limit = 100, $period = 3600)
+    public function __construct(StorageInterface $storage, array $options = [])
     {
         $this->storage = $storage;
-        $this->root = $root;
-        $this->limit = $limit;
-        $this->period = $period;
+        $this->setOptions($options);
+    }
+
+    private function setOptions(array $options)
+    {
+        foreach($options as $option => $value) {
+            if (in_array($option, ['root', 'limit', 'period', 'successCallback', 'failCallback'])) {
+                $this->{$option} = $value;
+            }
+        }
     }
 
     public function call()
     {
-        $response = $this->app->response;
-        $request = $this->app->request;
-
         // Activate on given root URL only
-        if (preg_match(
-            '|^' . $this->root . '.*|',
-            $request->getResourceUri()
-        )) {
-            if ($key = $this->app->config('rate.limit.key')) {
-                $data = $this->fetch($key);
-                if (!$data) {
-                    // First time or previous period expired,
-                    // initialize and save a new entry
-                    $remaining = $this->limit-1;
-                    $reset = $this->period;
-
-                    $this->save(
-                        $key,
-                        array(
-                            'remaining' => $remaining,
-                            'created' => time()
-                        ),
-                        $reset
-                    );
-                } else {
-                    // Take the current entry and update it
-                    $remaining = (--$data['remaining'] >= 0)
-                        ? $data['remaining'] : -1;
-
-                    $reset = (($data['created'] + $this->period) - time());
-
-                    if ($reset > 0) {
-                        $this->save(
-                            $key,
-                            array(
-                                'remaining' => $remaining,
-                                'created' => $data['created']
-                            ),
-                            $reset
-                        );
-                    } else {
-                        $this->delete($key);
-                    }
-                }
-
-                // Set rating headers
-                $response->headers->set(
-                    'X-Rate-Limit-Limit',
-                    $this->limit
-                );
-
-                $response->headers->set(
-                    'X-Rate-Limit-Reset',
-                    $reset
-                );
-                $response->headers->set(
-                    'X-Rate-Limit-Remaining',
-                    $remaining
-                );
-
-                // Check if the current key is allowed to pass
-                if (0 > $remaining) {
-                    // Rewrite remaining headers
-                    $response->headers->set(
-                        'X-Rate-Limit-Remaining',
-                        0
-                    );
-
-                    // Exits with status "429 Too Many Requests" (see doc below)
-                    $this->fail();
-                }
-
-            } else {
-                // Exits with status "429 Too Many Requests" (see doc below)
-                $this->fail();
-            }
+        if ($this->checkURL()) {
+            $this->checkLimit();
         }
-        $this->next->call();
 
+        $this->next->call();
+    }
+
+    protected function checkURL()
+    {
+        return preg_match(
+            '|^' . $this->root . '.*|',
+            $this->app->request->getResourceUri()
+        );
+    }
+
+    protected function checkLimit()
+    {
+        if (!$key = $this->app->config('rate.limit.key')) {
+            $key = '_rngr_ratelimit';
+        }
+
+        if (!$this->isValid($key)) {
+            return $this->fail();
+        } else {
+            return $this->success();
+        }
+    }
+
+    protected function isValid($key)
+    {
+        $data = $this->fetch($key);
+
+        if (!$data) {
+            $data = $this->addData($key);
+        } else {
+            $data = $this->updateData($key, $data);
+        }
+
+        if ($data['remaining'] >= 0 || $this->reset <= 0) {
+            $valid = true;
+        } else {
+            $valid = false;
+        }
+
+        $this->setHeaders($data);
+
+        return $valid;
+    }
+
+    protected function setHeaders($data)
+    {
+        // Set rating headers
+        $this->app->response->headers->set(
+            'X-Rate-Limit-Limit',
+            $this->limit
+        );
+
+        $this->app->response->headers->set(
+            'X-Rate-Limit-Reset',
+            $this->reset
+        );
+
+        $this->app->response->headers->set(
+            'X-Rate-Limit-Remaining',
+            $this->remaining < 0 ? 0 : $this->remaining
+        );
+    }
+
+    /**
+     * First time or previous period expired,
+     * initialize and save a new entry
+     * @param $key
+     * @param $nextCall
+     * @return array
+     */
+    protected function addData($key, $nextCall = false)
+    {
+        $this->reset = $this->period;
+        $this->remaining = $this->limit;
+
+        if (!$nextCall) {
+            $this->remaining -= 1;
+        }
+
+        $data = array(
+            'remaining' => $this->remaining,
+            'created' => time()
+        );
+
+        $this->save($key, $data, $this->reset);
+
+        return $data;
+    }
+
+    /**
+     * Take the current entry and update it
+     * @param $key
+     * @param $currentData
+     * @return bool
+     */
+    protected function updateData($key, $currentData)
+    {
+        $this->remaining = (--$currentData['remaining'] > 0) ? $currentData['remaining'] : -1;
+        $this->reset = (($currentData['created'] + $this->period) - time());
+        $data = array(
+            'remaining' => $this->remaining,
+            'created' => $currentData['created']
+        );
+
+        if ($this->reset > 0) {
+            $this->save($key, $data, $this->reset);
+        } else {
+            $this->delete($key);
+            $data = $this->addData($key, true);
+        }
+
+        return $data;
     }
 
     protected function fetch($key)
@@ -120,6 +170,13 @@ class RateLimit extends Middleware
         $this->storage->delete($key);
     }
 
+    public function success()
+    {
+        if (is_callable($this->successCallback)) {
+            return call_user_func_array([$this, 'successCallback'], [$this->app]);
+        }
+    }
+
     /**
      * Exits with status "429 Too Many Requests"
      *
@@ -129,6 +186,15 @@ class RateLimit extends Middleware
      * @link http://stackoverflow.com/questions/17735514/php-apache-silently-converting-http-429-and-others-to-500
      */
     protected function fail()
+    {
+        if (is_callable($this->failCallback)) {
+            return call_user_func_array([$this, 'failCallback'], [$this->app]);
+        }
+
+        return $this->defaultFailCallback();
+    }
+
+    protected function defaultFailCallback()
     {
         header('HTTP/1.1 429 Too Many Requests', false, 429);
 
